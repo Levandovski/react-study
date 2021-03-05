628180048378120df5a2ab7bdd6d48f34b685238
Jesse Levandovski <jessel@triadsystems.com.br>
2021-03-05 17:36:27 -0300

    Upload do projeto

---
+++ src/components/ComParametros.jsx
@@ -0,0 +1,8 @@
+import React from 'core-js/library/fn/reflect/es7/metadata'
+
+
+export default () =>
+    <>
+        <h3>Titulo</h3>
+        <p>Subtitulo</p>
+    </>
---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+export default  () =>  
+        <React.Fragment>
+            <h1>Primeiro Component (Arrow)</h1>
+            <h2>Exemplo de um component React</h2>
+        </React.Fragment>
+       
+
+
