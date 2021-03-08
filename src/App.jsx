697ea84489ad3408b4411bee055af666e0cbce39
Jesse Levandovski <jessel@triadsystems.com.br>
2021-03-08 16:23:12 -0300

    Nova implementação da ferramenta

--- src/App.jsx
+++ src/App.jsx
@@ -11,44 +11,50 @@ import CondicionalComIF from './components/basicos/CondicionalComIf';
 import Pai from './components/comunicacao/direta/Pai';
 import Super from './components/comunicacao/indireta/Super';
 import Input from './components/form/Input';
-
+import Contador from './components/contador/Contador';
 
 export default (props) =>(
     
-    <div className="App">        
-        <Card titulo="#09 Form Input" color="#FA6900">
-            <Input />
-        </Card>
-        <Card titulo="#08 Comunicação Indireta" color="#FA6900">
-            <Super />
-        </Card>
-        <Card titulo="#07 Comunicação Direta" color="#FA6900">
-            <Pai sobrenome='Freitas'/>
-        </Card>
-        <Card titulo="#06 Componente Com Condicional v2" color="#FA6900">
-            <CondicionalComIF numero={12}/>
-        </Card>
-        <Card titulo="#05 Componente Com Condicional v1" color="#E94C6F">
-            <Condicional numero={3}/>
-        </Card>
-        <Card titulo="#04 Componente Com Repetição" color="#E94C6F">
-            <Repeticao/>
-        </Card> 
-        <Card titulo="#03 Componente Com Filhos" color="#E94C6F">
-            <ComFilhos>
-            <ul>
-                <li>Carlos</li>
-                <li>José</li>
-                <li>Lucas</li>
-            </ul>
-            </ComFilhos>
-        </Card>   
-        <Card titulo="#02 Componente Com Parametro" color="#E94C6F">
-            <ComParametros titulo="Titulo 1" subtitulo="Subtitulo 2"/>  
-            <ComParametros titulo="Olá" subtitulo="Opa" />
-        </Card>
-        <Card titulo="#01 Primero Componente" color="#E94C6F">
-            <Primeiro />
-        </Card>       
+    <div className="App"> 
+        <h1>Fundamentos do React</h1>     
+        <div className="Cards">
+            <Card titulo="#10 Contador" color="#FA6900">
+                <Contador />
+            </Card>
+            <Card titulo="#09 Form Input" color="#FA6900">
+                <Input />
+            </Card>
+            <Card titulo="#08 Comunicação Indireta" color="#FA6900">
+                <Super />
+            </Card>
+            <Card titulo="#07 Comunicação Direta" color="#FA6900">
+                <Pai sobrenome='Freitas'/>
+            </Card>
+            <Card titulo="#06 Componente Com Condicional v2" color="#FA6900">
+                <CondicionalComIF numero={12}/>
+            </Card>
+            <Card titulo="#05 Componente Com Condicional v1" color="#E94C6F">
+                <Condicional numero={3}/>
+            </Card>
+            <Card titulo="#04 Componente Com Repetição" color="#E94C6F">
+                <Repeticao/>
+            </Card> 
+            <Card titulo="#03 Componente Com Filhos" color="#E94C6F">
+                <ComFilhos>
+                <ul>
+                    <li>Carlos</li>
+                    <li>José</li>
+                    <li>Lucas</li>
+                </ul>
+                </ComFilhos>
+            </Card>   
+            <Card titulo="#02 Componente Com Parametro" color="#E94C6F">
+                <ComParametros titulo="Titulo 1" subtitulo="Subtitulo 2"/>  
+                <ComParametros titulo="Olá" subtitulo="Opa" />
+            </Card>
+            <Card titulo="#01 Primero Componente" color="#E94C6F">
+                <Primeiro />
+            </Card>       
+        </div>       
     </div>
 );
---
+++ src/components/contador/Contador.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Contador extends Component{
+     //Component baseado em classe
+
+     render(){
+         return (
+             <div>
+                 <h2>Contador</h2>
+                 <h4>Display</h4>
+             </div>
+             
+         );
+     }
+}
