2e16afca53e1c3dacb62f9a19dbf936fb03c491c
Jesse Levandovski <jessel@triadsystems.com.br>
2021-03-08 10:28:31 -0300

    Nova implementação de estudos

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import './App.css';
 import React from 'react';
-import Primeiro from './components/Primeiro';
-import ComParametros from './components/ComParametros';
-import ComFilhos from './components/ComFilhos';
+import Primeiro from './components/basicos/Primeiro';
+import ComParametros from './components/basicos/ComParametros';
+import ComFilhos from './components/basicos/ComFilhos';
 import Card from './components/layout/Card';
-import Repeticao from './components/Repeticao';
-import Condicional from './components/Condicional';
-import CondicionalComIF from './components/CondicionalComIf';
+import Repeticao from './components/basicos/Repeticao';
+import Condicional from './components/basicos/Condicional';
+import CondicionalComIF from './components/basicos/CondicionalComIf';
 
 
 export default (props) =>(
     <div className="App">
-        <Card titulo="#06 Componente Com Condicional v2">
+        <Card titulo="#06 Componente Com Condicional v2" color="#FA6900">
             <CondicionalComIF numero={12}/>
         </Card>
-        <Card titulo="#05 Componente Com Condicional v1">
+        <Card titulo="#05 Componente Com Condicional v1" color="#E94C6F">
             <Condicional numero={3}/>
         </Card>
-        <Card titulo="#04 Componente Com Repetição">
+        <Card titulo="#04 Componente Com Repetição" color="#E94C6F">
             <Repeticao/>
         </Card> 
-        <Card titulo="#03 Componente Com Filhos">
+        <Card titulo="#03 Componente Com Filhos" color="#E94C6F">
             <ComFilhos>
             <ul>
                 <li>Carlos</li>
@@ -29,11 +29,11 @@ export default (props) =>(
             </ul>
             </ComFilhos>
         </Card>   
-        <Card titulo="#02 Componente Com Parametro">
+        <Card titulo="#02 Componente Com Parametro" color="#E94C6F">
             <ComParametros titulo="Titulo 1" subtitulo="Subtitulo 2"/>  
             <ComParametros titulo="Olá" subtitulo="Opa" />
         </Card>
-        <Card titulo="#01 Primero Componente">
+        <Card titulo="#01 Primero Componente" color="#E94C6F">
             <Primeiro />
         </Card>       
     </div>
--- src/components/basicos/Repeticao.jsx
+++ src/components/basicos/Repeticao.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import produtos from '../data/produtos'
+import produtos from '../../data/produtos'
 
 export default props =>{
 
     function getProdutosListItem(){
         return produtos.map(prod=>{
             return <li key={prod.id}>
-                    {prod.id} - {prod.nome} -> R$ {prod.preco}
+                    {prod.id} - {prod.nome} - R$ {prod.preco}
                   </li>
         }) 
     }
